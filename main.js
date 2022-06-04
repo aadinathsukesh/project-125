d9cad3f3e24243033cd4e203fd8fd3858f08404b
aadinathsukesh <85382361+aadinathsukesh@users.noreply.github.com>
2022-06-04 20:46:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+leftWrist_x=0;
+rightWrist_x=0;
+difference=0;
+
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(400,400);
+    video.position(10,50);
+
+    canvas = createCanvas(800,400);
+    canvas.position(430,130);
+
+    poseNet = ml5.poseNet(video,modelDone);
+    poseNet.on('pose',gotposes);
+}
+
+function draw(){
+    background("#ADD8E6");
+    document.getElementById("font_size").innerHTML="font size of the text will be ="+difference+"px";
+    textSize(difference);
+    fill("#0000FF");
+    text('prj-125',40,300);
+
+}
+
+function modelDone(){
+    console.log("PoseNet Is Initialized And Loaded");
+}
+
+function gotposes(results,error){
+    if(error){
+        console.error(error);
+    }
+    if(results.length > 0){
+        console.log(results);
+
+        leftWrist_x=results[0].pose.leftWrist.x;
+        rightWrist_x=results[0].pose.rightWrist.x;
+
+        difference=floor(leftWrist_x-rightWrist_x);
+
+        console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
+        console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
+    }
+}
